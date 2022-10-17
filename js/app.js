61e165531cc75ea3749614678a713c8a51f6cd0c
Rakib099 <rakibnabi97@gmail.com>
2022-10-17 13:15:59 +0600

    news found message added

--- js/app.js
+++ js/app.js
@@ -13,7 +13,8 @@ const displayCategories = categories => {
         a.classList.add('nav-link');
         a.classList.add('fs-5');
         a.setAttribute('href', '#');
-        a.setAttribute('onclick', `loadNews('${category.category_id}')`);
+        a.setAttribute('onclick', `loadNews('${category.category_id}', 
+        '${category.category_name}')`);
         a.innerText = `${category.category_name}`;
 
         categoriesContainer.appendChild(a);
@@ -22,17 +23,37 @@ const displayCategories = categories => {
     // console.log(categories);
 }
 
-const loadNews = id => {
+const loadNews = (id, categoryName) => {
     const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => displayNews(data.data))
+        .then(data => displayNews(data.data, categoryName))
         .catch(error => console.log(error))
 }
 
-const displayNews = news => {
+const displayNews = (news, categoryName) => {
     const newsContainer = document.getElementById('news-container');
     newsContainer.textContent = '';
+
+    // News Found Message
+    const messageContainer = document.getElementById('result-message-container');
+    const resultMessage = document.getElementById('result-message');
+    
+    if (news.length === 0) {
+        messageContainer.classList.remove('d-none');
+        messageContainer.classList.remove('text-bg-primary');
+        messageContainer.classList.add('bg-white');
+        resultMessage.innerText = `No News Found for category ${categoryName}`;
+    }
+    else if (news.length > 0) {
+        messageContainer.classList.remove('d-none');
+        messageContainer.classList.remove('bg-white');
+        messageContainer.classList.add('text-bg-primary');
+        resultMessage.innerText = `${news.length} news found for category ${categoryName}`;
+    }
+    else {
+        messageContainer.classList.add('d-none');
+    }
     news.forEach(singleNews => {
         const newsCard = document.createElement('div');
         newsCard.classList.add('col');
@@ -50,7 +71,7 @@ const displayNews = news => {
                             <div class="author-details d-flex gap-2">
                                 <img src="${singleNews.author.img}" class="author-img rounded-circle" alt="author-image">
                                 <div class="author-info">
-                                    <p class="mb-0">${singleNews.author.name}</p>
+                                    <p class="mb-0">${singleNews.author.name? singleNews.author.name : "No Data Available"}</p>
                                     <p class="text-secondary fs-6">
                                     ${singleNews.author.published_date}</p>
                                 </div>
@@ -58,7 +79,7 @@ const displayNews = news => {
 
                             <div class="view-count d-flex align-items-center gap-2">
                                 <i class="fa-regular fa-eye"></i>
-                                <h6 class="mb-0">${singleNews.total_view}</h6>
+                                <h6 class="mb-0">${singleNews.total_view? singleNews.total_view : "No Data Available"}</h6>
                             </div>
                             <i class="fa-solid fa-arrow-right text-primary fs-5"></i>
                         </div>
@@ -69,9 +90,9 @@ const displayNews = news => {
         `;
 
         newsContainer.appendChild(newsCard);
-        console.log(singleNews);
+        // console.log(singleNews);
     });
-    // console.log(news);
+    console.log(news, categoryName);
 }
 
-loadCategories();
+loadCategories();   // categories will be loaded by default
