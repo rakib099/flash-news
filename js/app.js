98e4d9981c0ee22c0ebf4be9b8c6c0fed0223b70
Rakib099 <rakibnabi97@gmail.com>
2022-10-17 15:55:16 +0600

    news cards improved

--- js/app.js
+++ js/app.js
@@ -58,36 +58,36 @@ const displayNews = (news, categoryName) => {
         const newsCard = document.createElement('div');
         newsCard.classList.add('col');
         newsCard.innerHTML = `
-            <div class="card">
+        <div class="card">
             <div class="row g-0">
                 <div class="col-md-4 p-3">
                     <img src="${singleNews.thumbnail_url}" class="img-fluid rounded-start" alt="...">
                 </div>
-                <div class="col-md-8">
+                <div class="col-md-8 d-flex flex-column">
                     <div class="card-body">
                         <h5 class="card-title">${singleNews.title}</h5>
                         <p class="card-text">${singleNews.details.length === 700 ? singleNews.details : singleNews.details.slice(0, 700)}</p>
-                        <div class="card-info d-flex align-items-center justify-content-between">
-                            <div class="author-details d-flex gap-2">
-                                <img src="${singleNews.author.img}" class="author-img rounded-circle" alt="author-image">
-                                <div class="author-info">
-                                    <p class="mb-0">${singleNews.author.name? singleNews.author.name : "No Data Available"}</p>
-                                    <p class="text-secondary fs-6">
-                                    ${singleNews.author.published_date}</p>
-                                </div>
-                            </div>
-
-                            <div class="view-count d-flex align-items-center gap-2">
-                                <i class="fa-regular fa-eye"></i>
-                                <h6 class="mb-0">${singleNews.total_view? singleNews.total_view : "No Data Available"}</h6>
-                            </div>
-
-                            <!-- Button trigger modal -->
-                            <i onclick="loadDetails('${singleNews._id}')" class="fa-solid fa-arrow-right text-primary fs-5" data-bs-toggle="modal" data-bs-target="#newsDetailsModal"></i>
-
-                            
-                        </div>
                     </div>
+
+                    <!-- Card Footer -->
+                    <div class="card-info d-flex align-items-center justify-content-between p-3">
+                        <div class="author-details d-flex gap-2">
+                            <img src="${singleNews.author.img}" class="author-img rounded-circle" alt="author-image">
+                            <div class="author-info">
+                                <p class="mb-0">${singleNews.author.name? singleNews.author.name : "No Data Available"}</p>
+                                <p class="text-secondary fs-6">
+                                ${singleNews.author.published_date}</p>
+                            </div>
+                        </div>
+                        <div class="view-count d-flex align-items-center gap-2">
+                            <i class="fa-regular fa-eye"></i>
+                            <h6 class="mb-0">${singleNews.total_view? singleNews.total_view : "No Data Available"}</h6>
+                        </div>
+
+                        <!-- Button trigger modal -->
+                        <i onclick="loadDetails('${singleNews._id}')" class="fa-solid fa-arrow-right text-primary fs-5" data-bs-toggle="modal" data-bs-target="#newsDetailsModal"></i>
+                    </div>
+
                 </div>
             </div>
         </div>
